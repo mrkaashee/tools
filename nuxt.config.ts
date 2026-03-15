1ba12363d15c2e276cddd6369d76393fd3d8fd79
mrkaashee <mrkaashee@gmail.com>
2026-03-15 15:49:05 +0500

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ export default defineNuxtConfig({
   modules: ['@nuxt/ui', '@nuxt/eslint', '@nuxt/hints'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  ui: {
+    content: true,
+    experimental: { componentDetection: true }
+  },
   compatibilityDate: '2026-03-15',
   eslint: { config: { stylistic: true } },
 })
