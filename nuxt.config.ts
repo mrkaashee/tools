469537a5f26dbe4134e4de77b5698ab5b3f942e0
mrkaashee <mrkaashee@gmail.com>
2026-03-22 16:54:23 +0500

    up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  extends: [
+    './layers/image',
+  ],
   modules: [
     '@nuxt/ui',
     '@nuxt/eslint',
